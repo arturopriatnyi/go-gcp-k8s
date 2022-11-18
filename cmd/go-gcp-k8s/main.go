39fcdda67d1b192720cfc3d578d925ba138b4f1b
Arthur Opriatnyi <arthur.opriatnyi@emodoinc.com>
2022-11-18 13:51:20 +0200

    update response message

--- cmd/go-gcp-k8s/main.go
+++ cmd/go-gcp-k8s/main.go
@@ -21,7 +21,7 @@ func main() {
 		Addr: ":10000",
 		Handler: http.HandlerFunc(
 			func(w http.ResponseWriter, _ *http.Request) {
-				if _, err := w.Write([]byte("Hello from GCP Kubernetes!")); err != nil {
+				if _, err := w.Write([]byte("UPD: Hello from GCP Kubernetes!")); err != nil {
 					l.Printf("response writing failed: %v", err)
 				}
 			},
